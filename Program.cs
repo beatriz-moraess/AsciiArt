eed837bdbbd5a7842754cc6c9dfc5c779d9eb2ab
Beatriz <beatriz.giovanna@gmail.com>
2025-03-19 15:38:29 -0300

    finaliza programa

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Clear();
+
+Console.ForegroundColor = ConsoleColor.DarkCyan;
+
+Console.WriteLine( "     \\`-._           __                   ");
+Console.WriteLine( "      \\  `-..____,.'  `.                  ");
+Console.WriteLine( "       :`.         /    \\`.               ");
+Console.WriteLine( "       :  )       :      : \\              ");
+Console.WriteLine( "        ;'        '   ;  |  :              ");
+Console.WriteLine( "        )..      .. .:.`.;  :              ");
+Console.WriteLine( "       /::...  .:::...   ` ;               ");
+Console.WriteLine( "        ; _ '    __        /:\\            ");
+Console.WriteLine( "        `:o>   /\\o_>      ;:. `.)         ");
+Console.WriteLine( "       `-`.__ ;   __..--- /:.   \\         ");
+Console.WriteLine( "       === \\_/   ;=====_.':.     ;        ");
+Console.WriteLine( "        ,/'`--'...`--....        ;         ");
+Console.WriteLine( "             ;                    ;        ");
+Console.WriteLine( "           .'                      ;       ");
+Console.WriteLine( "         .'                        ;       ");
+Console.WriteLine( "       .'     ..     ,      .       ;      ");
+Console.WriteLine( "      :       ::..  /      ;::.     |      ");
+Console.WriteLine( "     /      `.;::.  |       ;:..    ;      ");
+Console.WriteLine( "    :         |:.   :       ;:.    ;       ");
+Console.WriteLine( "    :         ::     ;:..   |.    ;        ");
+Console.WriteLine( "     :       :;      :::....|     |        ");
+Console.WriteLine( "     /\\     ,/ \\      ;:::::;     ;      ");
+Console.WriteLine( "   .:. \\:..|    :     ; '.--|     ;       ");
+Console.WriteLine( "  ::.  :''  `-.,,;     ;'   ;     ;        ");
+Console.WriteLine( ".-'. _.'\\      / `;      \\,__:      \\   ");
+Console.WriteLine(" `---'    `----'   ;      /    \\,.,,,/     ");
+Console.WriteLine("                   `----`                   ");   
+
+Console.ResetColor();
